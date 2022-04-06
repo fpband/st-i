446485cc97738ca902b9c3da9b6dd021e33832ef
fpband <82159763+fpband@users.noreply.github.com>
2022-04-06 10:46:37 +0430

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,8 +90,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("😊 درباره ی من ", callback_data = "about"),
+                    InlineKeyboardButton("🔒 خروج", callback_data = "close")
                 ]
             ]
         )
